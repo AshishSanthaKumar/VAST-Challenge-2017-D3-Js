60446b02f9dce961640eab5c8c87cfe96c87e354
Venkatramanan Srinivasan <venkyman@Venkat.local>
2020-11-20 00:22:58 -0700

    Heatmap working for one date

--- scripts/script.js
+++ scripts/script.js
@@ -146,9 +146,11 @@ function drawTimeline() {
     drawAllCharts();
 }
 
-function drawAllCharts() {
-    drawColumnChart();
-    drawRadialChart();
+function drawAllCharts(startDate,endDate)
+{
+    drawColumnChart(startDate,endDate);
+    drawRadialChart(startDate,endDate);
+    drawHeatMap(startDate,endDate);
 }
 
 
@@ -559,4 +561,106 @@ console.log(selectedSensor);
 
 
 
+}
+
+function drawHeatMap(startDate,endDate){
+    // d3.select("#heatmap").selectAll("*").remove();
+    
+    var startdateString = new Date(startDate.getTime() - (startDate.getTimezoneOffset() * 60000 ))
+                    .toISOString()
+                    .split("T")[0];
+    console.log(startdateString);
+    var margin = { top: 50, right: 0, bottom: 100, left: 30 },
+          width = 960 - margin.left - margin.right,
+          height = 430 - margin.top - margin.bottom,
+          gridSize = Math.floor(width / 50),
+          legendElementWidth = gridSize*2,
+          buckets = 9,
+          colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+          days = ["AGOC-3A", "Appluimonia", "Chlorodinine", "Methylosmolene"],
+          times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+          datasets = ["../MC2Data/sensor_1_new.json"];
+
+          console.log(d3.schemeBlues);
+    d3.select("#heatmap").selectAll("*").remove();
+      var svg = d3.select("#heatmap").append("svg")
+          .attr("width", width + margin.left + margin.right +100)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + (margin.left + 100)  + "," + margin.top + ")");
+
+      var dayLabels = svg.selectAll(".dayLabel")
+          .data(days)
+          .enter().append("text")
+            .text(function (d) { return d; })
+            .attr("x", 0)
+            .attr("y", function (d, i) { return i * gridSize; })
+            .style("text-anchor", "end")
+            .attr("transform", "translate(-10," + gridSize / 1.5 + ")")
+            .attr("class", "dayLabel mono axis axis-workweek");
+
+      var timeLabels = svg.selectAll(".timeLabel")
+          .data(times)
+          .enter().append("text")
+            .text(function(d) { return d; })
+            .attr("x", function(d, i) { return i * gridSize; })
+            .attr("y", 0)
+            .style("font", "10px times")
+            .style("text-anchor", "middle")
+            .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+            .attr("class", "timeLabel mono axis axis-worktime");
+
+      
+      d3.json(datasets[0]).then(
+
+        function(data) {
+
+          console.log(data);
+          var colorScale = d3.scaleQuantile()
+              .domain([0, buckets - 1, d3.max(data[startdateString], function (d) { return (d.value); })])
+              .range(colors);
+
+              
+          var cards = svg.selectAll(".hour")
+              .data(data[startdateString]);
+
+          cards.append("title");
+
+          cards.enter().append("rect")
+              .attr("x", function(d,i) { return ((d.hour) - 1) * gridSize; })
+              .attr("y", function(d) { return ((d.chemical) - 1) * gridSize; })
+              .attr("rx", 4)
+              .attr("ry", 4)
+              .attr("class", "hour bordered")
+              .attr("width", gridSize)
+              .attr("height", gridSize)
+              .transition().duration(500)
+              .style("fill", function(d) { return colorScale((d.value)); });
+
+          cards.select("title").text(function(d) { return (d.value); });
+          
+          cards.exit().remove();
+
+          // var legend = svg.selectAll(".legend")
+          //     .data([0].concat(colorScale.quantiles()), function(d) { return d; });
+
+          // legend.enter().append("g")
+          //     .attr("class", "legend");
+
+          // legend.append("rect")
+          //   .attr("x", function(d, i) { return legendElementWidth * i; })
+          //   .attr("y", height)
+          //   .attr("width", legendElementWidth)
+          //   .attr("height", gridSize / 2)
+          //   .style("fill", function(d, i) { return colors[i]; });
+
+          // legend.append("text")
+          //   .attr("class", "mono")
+          //   .text(function(d) { return "≥ " + Math.round(d); })
+          //   .attr("x", function(d, i) { return legendElementWidth * i; })
+          //   .attr("y", height + gridSize);
+
+          // legend.exit().remove();
+
+        });  
 }
